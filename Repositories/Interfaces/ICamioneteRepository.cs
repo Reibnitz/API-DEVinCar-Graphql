47561dc5c220a189ba5981131d494409d0111855
Lucas Reibnitz <lucas.fpolis@gmail.com>
2022-10-10 22:14:42 -0300

    refactor: move file location

---
+++ Repositories/Interfaces/ICamioneteRepository.cs
@@ -0,0 +1,8 @@
+﻿using API_DEVinCar_Graphql.Models;
+
+namespace API_DEVinCar_Graphql.Repositories.Interfaces
+{
+    public interface ICamioneteRepository : IRepository<Camionete>
+    {
+    }
+}
---
+++ Repositories/Interfaces/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using API_DEVinCar_Graphql.Models;
+
+namespace API_DEVinCar_Graphql.Repositories.Interfaces
+{
+    public interface IRepository<T>
+    {
+        Task<List<T>> GetAll();
+        Task<List<T>> GetAvailable();
+        Task<List<T>> GetSold();
+        Task<bool> Sell(Guid id);
+        Task<bool> ChangeColor(Guid id, string color);
+        Task<bool> ChangePrice(Guid id, double price);
+        Task<bool> Add(T entity);
+    }
+}
