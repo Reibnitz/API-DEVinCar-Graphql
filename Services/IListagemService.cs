8cb57f08fa9febee011b9d33845bbf7058a4d731
Lucas Reibnitz <lucas.fpolis@gmail.com>
2022-10-10 22:15:30 -0300

    feat: add method Disponiveis

---
+++ Services/IListagemService.cs
@@ -0,0 +1,10 @@
+﻿using API_DEVinCar_Graphql.Data.Enums;
+using API_DEVinCar_Graphql.Data.Models;
+
+namespace API_DEVinCar_Graphql.Services
+{
+    public interface IListagemService
+    {
+        Task<IEnumerable<Veiculo>> Disponiveis(EVeiculo? tipoVeiculo);
+    }
+}
---
+++ Services/ListagemService.cs
@@ -0,0 +1,58 @@
+﻿using API_DEVinCar_Graphql.Data.Enums;
+using API_DEVinCar_Graphql.Data.Models;
+using API_DEVinCar_Graphql.Models;
+using API_DEVinCar_Graphql.Repositories.Interfaces;
+
+namespace API_DEVinCar_Graphql.Services
+{
+    public class ListagemService : IListagemService
+    {
+        private readonly ICamioneteRepository _camioneteRepository;
+        private readonly ICarroRepository _carroRepository;
+        private readonly IMotoTricicloRepository _motoRepository;
+
+        public ListagemService(ICamioneteRepository camioneteRepository, ICarroRepository carroRepository, IMotoTricicloRepository motoRepository)
+        {
+            _camioneteRepository = camioneteRepository;
+            _carroRepository = carroRepository;
+            _motoRepository = motoRepository;
+        }
+
+        public async Task<IEnumerable<Veiculo>> Disponiveis(EVeiculo? tipoVeiculo)
+        {
+            List<Veiculo> veiculos = new();
+            List<Carro> carros = new();
+            List<Camionete> camionetes = new();
+            List<MotoTriciclo> motos = new();
+
+            switch (tipoVeiculo)
+            {
+                case EVeiculo.Carro:
+                    carros = await _carroRepository.GetAvailable();
+                    carros.ForEach(c => veiculos.Add(c));
+                    break;
+
+                case EVeiculo.Camionete:
+                    camionetes = await _camioneteRepository.GetAvailable();
+                    camionetes.ForEach(c => veiculos.Add(c));
+                    break;
+
+                case EVeiculo.MotoTriciclo:
+                    motos = await _motoRepository.GetAvailable();
+                    motos.ForEach(m => veiculos.Add(m));
+                    break;
+
+                default:
+                    carros = await _carroRepository.GetAvailable();
+                    carros.ForEach(c => veiculos.Add(c));
+                    camionetes = await _camioneteRepository.GetAvailable();
+                    camionetes.ForEach(c => veiculos.Add(c));
+                    motos = await _motoRepository.GetAvailable();
+                    motos.ForEach(m => veiculos.Add(m));
+                    break;
+            }
+
+            return veiculos;
+        }
+    }
+}
