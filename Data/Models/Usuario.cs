4f9e5767e7b38c9203ca25002c1ec690feb23800
Lucas Reibnitz <lucas.fpolis@gmail.com>
2022-10-13 20:47:21 -0300

    feat: create usuario

---
+++ Data/Models/Usuario.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace API_DEVinCar_Graphql.Data.Models
+{
+    public class Usuario
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int? Id { get; set; }
+
+        [Required(ErrorMessage = "Campo Username de preenchimento obrigatório")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Campo Password de preenchimento obrigatório")]
+        [MinLength(6)]
+        public string Password { get; set; }
+    }
+}
