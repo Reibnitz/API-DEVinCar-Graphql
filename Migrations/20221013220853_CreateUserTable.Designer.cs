fc8902069c87c338226f4e6c4cc3cb3e495a8fda
Lucas Reibnitz <lucas.fpolis@gmail.com>
2022-10-13 20:48:26 -0300

    feat: create usuario table migration

--- Migrations/20221013220853_CreateUserTable.Designer.cs
+++ Migrations/20221013220853_CreateUserTable.Designer.cs
@@ -4,6 +4,7 @@ using API_DEVinCar_Graphql.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace API_DEVinCar_Graphql.Migrations
 {
     [DbContext(typeof(DEVInCarContext))]
-    partial class DEVInCarContextModelSnapshot : ModelSnapshot
+    [Migration("20221013220853_CreateUserTable")]
+    partial class CreateUserTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
