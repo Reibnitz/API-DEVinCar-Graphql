0b4f27df13478b109a548b53eed54bbd7b697ed1
Lucas Reibnitz <lucas.fpolis@gmail.com>
2022-10-12 21:33:53 -0300

    feat: create interfaces for mutation and query services

---
+++ Services/Interfaces/IQueriesService.cs
@@ -0,0 +1,12 @@
+﻿using API_DEVinCar_Graphql.Data.Enums;
+using API_DEVinCar_Graphql.Data.Models;
+
+namespace API_DEVinCar_Graphql.Services.Interfaces
+{
+    public interface IQueriesService
+    {
+        Task<IEnumerable<Veiculo>> GetAvailableAsync(EVeiculo? tipoVeiculo);
+        Task<IEnumerable<Veiculo>> GetSoldAsync(EVeiculo? tipoVeiculo);
+        Task<IEnumerable<Veiculo>> GetAllAsync(EVeiculo? tipoVeiculo);
+    }
+}
