292fb4e0b46cda09e8b73264676893dbb3b58f23
Lucas Reibnitz <lucas.fpolis@gmail.com>
2022-10-13 20:48:39 -0300

    feat: create login mutation

---
+++ Graphql/LoginMutation.cs
@@ -0,0 +1,16 @@
+﻿using API_DEVinCar_Graphql.Data.Models;
+using API_DEVinCar_Graphql.Services.Interfaces;
+
+namespace API_DEVinCar_Graphql.Graphql
+{
+    [ExtendObjectType(OperationTypeNames.Mutation)]
+    public class LoginMutation
+    {
+        [GraphQLName("Login")]
+        [GraphQLDescription("Retorna resultado da solicitação de login")]
+        public async Task<string> Login(Login login, [Service] IUsuarioMutationService service)
+        {
+            return await service.Login(login);
+        }
+    }
+}
