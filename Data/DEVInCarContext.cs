2a0ea4ba3df1ac61037c660b0e8e927f48c1018e
Lucas Reibnitz <lucas.fpolis@gmail.com>
2022-10-13 20:48:18 -0300

    feat: create usuario table

--- Data/DEVInCarContext.cs
+++ Data/DEVInCarContext.cs
@@ -16,12 +16,14 @@ namespace API_DEVinCar_Graphql.Data
         public DbSet<Camionete> Camionetes { get; set; }
         public DbSet<MotoTriciclo> MotoTriciclos { get; set; }
         public DbSet<Venda> Vendas { get; set; }
+        public DbSet<Usuario> Usuarios { get; set; }
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
             builder.Entity<Camionete>().HasData(CamioneteSeeder.Seed);
             builder.Entity<Carro>().HasData(CarroSeeder.Seed);
             builder.Entity<MotoTriciclo>().HasData(MotoTricicloSeeder.Seed);
+            builder.Entity<Usuario>().HasData(UsuarioSeeder.Seed);
         }
     }
 }
---
+++ Data/Seeds/UsuarioSeeder.cs
@@ -0,0 +1,29 @@
+﻿using API_DEVinCar_Graphql.Data.Models;
+
+namespace API_DEVinCar_Graphql.Data.Seeds
+{
+    public class UsuarioSeeder
+    {
+        public static List<Usuario> Seed = new()
+        {
+            new Usuario
+            {
+                Username = "Yan",
+                Password = "123456",
+                Id = 1
+            },
+            new Usuario
+            {
+                Username = "Fernando",
+                Password = "654321",
+                Id = 2
+            },
+            new Usuario
+            {
+                Username = "Lucas",
+                Password = "qwerty",
+                Id = 3
+            }
+        };
+    }
+}
