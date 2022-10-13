8ab81a355f6a9d6dc98859d353189d124795a320
Lucas Reibnitz <lucas.fpolis@gmail.com>
2022-10-12 21:28:55 -0300

    feat: add venda model

---
+++ Data/Models/Venda.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using API_DEVinCar_Graphql.Data.Enums;
+
+namespace API_DEVinCar_Graphql.Data.Models
+{
+    public class Venda
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid Id { get; set; }
+
+        [Required(ErrorMessage = "Campo VeiculoId de preenchimento obrigatório")]
+        public Guid VeiculoId { get; set; }
+
+        [Required(ErrorMessage = "Campo Cpf de preenchimento obrigatório")]
+        public string Cpf { get; set; }
+
+        [DataType(DataType.DateTime, ErrorMessage = "Formato de data inválido.")]
+        [Required(ErrorMessage = "Campo Data de preenchimento obrigatório")]
+        public DateTime Data { get; set; }
+
+        public EVeiculoVendido TipoVeiculo { get; set; }
+    }
+}
