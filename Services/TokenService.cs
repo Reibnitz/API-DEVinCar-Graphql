c9c2ad521018eed83b8baea2c8664ad6eb58503c
Lucas Reibnitz <lucas.fpolis@gmail.com>
2022-10-13 20:50:26 -0300

    feat: create token service

---
+++ Services/TokenService.cs
@@ -0,0 +1,41 @@
+﻿using API_DEVinCar_Graphql.Data.Models;
+using API_DEVinCar_Graphql.Services.Interfaces;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+
+namespace API_DEVinCar_Graphql.Services
+{
+    public class TokenService : ITokenService
+    {
+        private readonly IConfiguration _configuration;
+
+        public TokenService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public string CreateToken(Login login)
+        {
+            var issuedAt = DateTime.UtcNow;
+            var expires = issuedAt.AddHours(3);
+            var handler = new JwtSecurityTokenHandler();
+
+            var properties = new SecurityTokenDescriptor()
+            {
+                Issuer = _configuration.GetSection("TokenSettings").GetValue<string>("Issuer"),
+                IssuedAt = issuedAt,
+                Expires = expires,
+                Audience = _configuration.GetSection("TokenSettings").GetValue<string>("Audience"),
+                SigningCredentials = new SigningCredentials(
+                    new SymmetricSecurityKey(
+                        Encoding.UTF8.GetBytes(_configuration.GetSection("TokenSettings").GetValue<string>("Key"))
+                    ),
+                    SecurityAlgorithms.HmacSha256Signature
+                )
+            };
+
+            return handler.WriteToken(handler.CreateToken(properties));
+        }
+    }
+}
