6f376f8c52d65652e4aa7a0bb8e25740aac952a0
Lucas Reibnitz <lucas.fpolis@gmail.com>
2022-10-13 20:50:13 -0300

    feat: create usuario repository

---
+++ Repositories/UsuarioRepository.cs
@@ -0,0 +1,26 @@
+﻿using API_DEVinCar_Graphql.Data;
+using API_DEVinCar_Graphql.Data.Models;
+using API_DEVinCar_Graphql.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace API_DEVinCar_Graphql.Repositories
+{
+    public class UsuarioRepository : IUsuarioRepository
+    {
+        private readonly DEVInCarContext _context;
+
+        public UsuarioRepository(DEVInCarContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<bool> Login(Login login)
+        {
+            bool result = await _context.Usuarios.AnyAsync(u =>
+                u.Username == login.Username &&
+                u.Password == login.Password);
+
+            return result;
+        }
+    }
+}
