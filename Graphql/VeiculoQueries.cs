390ef41568504490de7b74fd401319bb5c0550ad
Lucas Reibnitz <lucas.fpolis@gmail.com>
2022-10-12 21:30:14 -0300

    refactor: rename async methods

--- Graphql/VeiculoQueries.cs
+++ Graphql/VeiculoQueries.cs
@@ -1,6 +1,6 @@
 ﻿using API_DEVinCar_Graphql.Data.Enums;
 using API_DEVinCar_Graphql.Data.Models;
-using API_DEVinCar_Graphql.Services;
+using API_DEVinCar_Graphql.Services.Interfaces;
 
 namespace API_DEVinCar_Graphql.Graphql
 {
@@ -9,31 +9,31 @@ namespace API_DEVinCar_Graphql.Graphql
     {
         [GraphQLName("Disponiveis")]
         [GraphQLDescription("Retorna lista de veículos disponíveis")]
-        public async Task<IEnumerable<Veiculo>> GetAvailable(EVeiculo? tipoVeiculo, [Service] IListagemService listagemService)
+        public async Task<IEnumerable<Veiculo>> GetAvailable(EVeiculo? tipoVeiculo, [Service] IQueriesService listagemService)
         {
-            return await listagemService.GetAvailable(tipoVeiculo);
+            return await listagemService.GetAvailableAsync(tipoVeiculo);
         }
 
         [GraphQLName("Vendidos")]
         [GraphQLDescription("Retorna lista de veículos vendidos")]
-        public async Task<IEnumerable<Veiculo>> GetSold(EVeiculo? tipoVeiculo, [Service] IListagemService listagemService)
+        public async Task<IEnumerable<Veiculo>> GetSold(EVeiculo? tipoVeiculo, [Service] IQueriesService listagemService)
         {
-            return await listagemService.GetSold(tipoVeiculo);
+            return await listagemService.GetSoldAsync(tipoVeiculo);
         }
 
         [GraphQLName("MaiorPreco")]
         [GraphQLDescription("Retorna lista de veículos ordenados pelo maior preço")]
-        public async Task<IEnumerable<Veiculo>> GetHighestPrices(EVeiculo? tipoVeiculo, [Service] IListagemService listagemService)
+        public async Task<IEnumerable<Veiculo>> GetHighestPrices(EVeiculo? tipoVeiculo, [Service] IQueriesService listagemService)
         {
-            IEnumerable<Veiculo> veiculos = await listagemService.GetAll(tipoVeiculo);
+            IEnumerable<Veiculo> veiculos = await listagemService.GetAllAsync(tipoVeiculo);
             return veiculos.ToList().OrderByDescending(v => v.Valor);
         }
 
         [GraphQLName("MenorPreco")]
         [GraphQLDescription("Retorna lista de veículos odernados pelo menor preço")]
-        public async Task<IEnumerable<Veiculo>> GetLowestPrices(EVeiculo? tipoVeiculo, [Service] IListagemService listagemService)
+        public async Task<IEnumerable<Veiculo>> GetLowestPrices(EVeiculo? tipoVeiculo, [Service] IQueriesService listagemService)
         {
-            IEnumerable<Veiculo> veiculos = await listagemService.GetAll(tipoVeiculo);
+            IEnumerable<Veiculo> veiculos = await listagemService.GetAllAsync(tipoVeiculo);
             return veiculos.ToList().OrderBy(v => v.Valor);
         }
     }
