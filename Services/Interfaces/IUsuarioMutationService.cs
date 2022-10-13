6999eb095b7547557d1460371165424523880103
Lucas Reibnitz <lucas.fpolis@gmail.com>
2022-10-13 20:50:36 -0300

    feat: create usuario mutation service

---
+++ Services/Interfaces/IUsuarioMutationService.cs
@@ -0,0 +1,9 @@
+﻿using API_DEVinCar_Graphql.Data.Models;
+
+namespace API_DEVinCar_Graphql.Services.Interfaces
+{
+    public interface IUsuarioMutationService
+    {
+        Task<string> Login(Login login);
+    }
+}
---
+++ Services/UsuarioMutationService.cs
@@ -0,0 +1,29 @@
+﻿using API_DEVinCar_Graphql.Data.Models;
+using API_DEVinCar_Graphql.Repositories.Interfaces;
+using API_DEVinCar_Graphql.Services.Interfaces;
+
+namespace API_DEVinCar_Graphql.Services
+{
+    public class UsuarioMutationService : IUsuarioMutationService
+    {
+        private readonly IUsuarioRepository _repository;
+        private readonly ITokenService _tokenService;
+
+        public UsuarioMutationService(IUsuarioRepository repository, ITokenService tokenService)
+        {
+            _repository = repository;
+            _tokenService = tokenService;
+        }
+
+        public async Task<string> Login(Login login)
+        {
+            bool result = await _repository.Login(login);
+            if (result == false)
+                return "Usuário e/ou senha incorretos";
+
+            string token = _tokenService.CreateToken(login);
+
+            return token;
+        }
+    }
+}
