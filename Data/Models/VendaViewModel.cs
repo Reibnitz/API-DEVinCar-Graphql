bc89fa32fd59d8c3b9e74555e913c705cc52272a
Lucas Reibnitz <lucas.fpolis@gmail.com>
2022-10-13 21:22:34 -0300

    feat: create venda view model

---
+++ Data/Models/VendaViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace API_DEVinCar_Graphql.Data.Models
+{
+    public class VendaViewModel
+    {
+        public Venda Venda { get; set; }
+        public Veiculo Veiculo { get; set; }
+    }
+}
